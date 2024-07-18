66ec642331cd8fd028b8b1d3a225bc5853d7b50f
guilhermetborges <112335378+guilhermetborges@users.noreply.github.com>
2024-07-18 01:46:43 -0300

    Add files via upload

---
+++ imparpar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main(){
+	int num;
+	printf("digite um numero: ");
+	scanf("%d", &num);
+	if(num % 2 == 0){
+		printf("o numero %d e par.",num);
+	}else{
+		printf("o numerp %d e impar.",num);
+	}                                                                                                                                            
+}
